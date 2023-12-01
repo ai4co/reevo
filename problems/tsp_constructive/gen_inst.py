b175fbc52745f90a912e3b80dde28185185b7713
Haoran-Ye <hrye@stu.suda.edu.cn>
2023-12-02 01:38:50 +0800

    update config

--- problems/tsp_constructive/gen_inst.py
+++ problems/tsp_constructive/gen_inst.py
@@ -4,7 +4,7 @@ import numpy as np
 np.random.seed(0)
 
 # generate ans save test instances for TSP
-for problem_size in [20, 50, 100]:
-    n_instances = 30
+for problem_size in [50]:
+    n_instances = 64
     test_dataset = np.random.rand(n_instances, problem_size, 2)
     np.save(f'./dataset/val{problem_size}_dataset.npy', test_dataset)
