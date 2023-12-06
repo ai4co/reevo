fdba9ae93003f723c9858d2b608e348b6fb1e501
Haoran-Ye <hrye@stu.suda.edu.cn>
2023-12-07 02:43:06 +0800

    Add diversify

--- ga.py
+++ ga.py
@@ -17,8 +17,14 @@ class GA_LLM:
         self.root_dir = root_dir
         
         self.iteration = 0
+        self.function_evals = 0
+            
         self.population = self.init_population()
 
+        if cfg.diversify:
+            self.greedy_obj = self.evaluate_greedy_alg()
+            logging.info(f"Greedy Algorithm Objective Value: {self.greedy_obj}")
+        
         self.ga_crossover_prompt = file_to_string(f'{root_dir}/utils/prompts_ga/crossover.txt')
         self.ga_mutate_prompt = file_to_string(f'{root_dir}/utils/prompts_ga/mutate.txt')
 
@@ -41,10 +47,10 @@ class GA_LLM:
         self.code_output_tip = file_to_string(f'{prompt_dir}/code_output_tip.txt')
         func_signature = file_to_string(f'{problem_dir}/func_signature.txt')
         self.system_prompt = system.format(func_signature=func_signature)
-        initial_user = initial_user.format(problem_description=self.problem_description) + self.code_output_tip
+        self.initial_user = initial_user.format(problem_description=self.problem_description)
 
         # Generate responses
-        messages = [{"role": "system", "content": self.system_prompt}, {"role": "user", "content": initial_user}]
+        messages = [{"role": "system", "content": self.system_prompt}, {"role": "user", "content": self.initial_user + self.code_output_tip}]
         responses = self.chat_completion(self.cfg.pop_size, self.cfg, messages)
         
         # Responses to population
@@ -64,16 +70,33 @@ class GA_LLM:
         self.iteration += 1
         return population
     
-    def response_to_individual(self, response, response_id) -> dict:
+    def evaluate_greedy_alg(self) -> float:
+        """
+        Generate and evaluate the greedy algorithm for the problem, e.g. Nearest Neighbor for TSP.
+        """
+        # Loading all text prompts
+        greedy_alg_prompt = file_to_string(f'{self.root_dir}/utils/prompts_ga/gen_greedy.txt')
+        messages = [{"role": "system", "content": self.system_prompt}, {"role": "user", "content": self.initial_user + greedy_alg_prompt}]
+        # Generate responses
+        responses = self.chat_completion(1, self.cfg, messages)
+        # Response to individual
+        individual = self.response_to_individual(responses[0], 0, file_name="greedy_alg")
+        # Run code and evaluate population
+        population = self.evaluate_population([individual])
+        return population[0]["obj"]
+    
+    def response_to_individual(self, response, response_id, file_name=None) -> dict:
         """
         Convert response to individual. Applied to crossover and mutation.
         """
         content = response.message.content
         # Write response to file
-        with open(f"problem_iter{self.iteration}_response{response_id}.txt", 'w') as file:
+        file_name = f"problem_iter{self.iteration}_response{response_id}.txt" if file_name is None else file_name + ".txt"
+        with open(file_name, 'w') as file:
             file.writelines(content + '\n')
 
         code_string, desc_string = self.extract_code_description(content)
+        std_out_filepath = f"problem_iter{self.iteration}_stdout{response_id}.txt" if file_name is None else file_name + "_stdout.txt"
         individual = {
             "stdout_filepath": f"problem_iter{self.iteration}_stdout{response_id}.txt",
             "code_path": f"problem_iter{self.iteration}_code{response_id}.py",
@@ -98,11 +121,12 @@ class GA_LLM:
 
     def evaluate_population(self, population: list[dict]) -> list[float]:
         """
-        Evaluate population by running code and computing objective values and fitness.
+        Evaluate population by running code in parallel and computing objective values and fitness.
         """
         inner_runs = []
         # Run code and evaluate population
         for response_id in range(len(population)):
+            self.function_evals += 1
             logging.info(f"Iteration {self.iteration}: Running Code {response_id}")
             try:
                 process = self.run_code(population[response_id], response_id)
@@ -170,7 +194,7 @@ class GA_LLM:
             file.writelines(individual["code"] + '\n')
             
         # Copy the generated code to hydra output directory for bookkeeping
-        shutil.copy(self.output_file, f"problem_iter{self.iteration}_code{response_id}.py")
+        # shutil.copy(self.output_file, f"problem_iter{self.iteration}_code{response_id}.py")
 
         # Execute the python file with flags
         with open(individual["stdout_filepath"], 'w') as f:
@@ -230,6 +254,7 @@ class GA_LLM:
             self.best_desc_overall = population[best_sample_idx]["description"]
             self.best_code_path_overall = population[best_sample_idx]["code_path"]
         logging.info(f"Iteration {self.iteration}: Min obj: {self.best_obj_overall}, Best Code Path: {self.best_code_path_overall}")
+        logging.info(f"Iteration {self.iteration}: Function Evals: {self.function_evals}")
         self.iteration += 1
 
     @staticmethod
@@ -314,8 +339,8 @@ class GA_LLM:
             crossover_prompt = self.ga_crossover_prompt.format(
                 code1=parent_1["code"], code2=parent_2["code"],
                 description1=parent_1["description"], description2=parent_2["description"],
-                ) + self.code_output_tip
-            messages = [{"role": "system", "content": self.system_prompt}, {"role": "user", "content": crossover_prompt}]
+                )
+            messages = [{"role": "system", "content": self.system_prompt}, {"role": "user", "content": crossover_prompt + self.code_output_tip}]
             responses = self.chat_completion(1, self.cfg, messages)
             # Response to individual
             individual = self.response_to_individual(responses[0], response_id)
@@ -331,8 +356,8 @@ class GA_LLM:
             individual = population[i]
             # Mutate
             if np.random.uniform() < self.cfg.mutation_rate:
-                mutate_prompt = self.ga_mutate_prompt.format(code=individual["code"], description=individual["description"]) + self.code_output_tip
-                messages = [{"role": "system", "content": self.system_prompt}, {"role": "user", "content": mutate_prompt}]
+                mutate_prompt = self.ga_mutate_prompt.format(code=individual["code"], description=individual["description"])
+                messages = [{"role": "system", "content": self.system_prompt}, {"role": "user", "content": mutate_prompt + self.code_output_tip}]
                 responses = self.chat_completion(1, self.cfg, messages)
                 # Response to individual
                 mutated_individual = self.response_to_individual(responses[0], individual["response_id"])
@@ -344,6 +369,8 @@ class GA_LLM:
 
     def evolve(self):
         while self.iteration < self.cfg.max_iter:
+            # Diversify
+            self.population = self.diversify(self.population) if self.cfg.diversify else self.population
             # Select
             selected_population = self.random_select(self.population)
             # Crossover
@@ -372,13 +399,31 @@ class GA_LLM:
         similarity_matrix = cosine_similarity(embeddings)
         return similarity_matrix
     
-
-
-if __name__ == '__main__':
-    # Example usage
-    code_snippets = [
-        "def add(a, b): return a + b",
-        "def subtract(a, b): return a - b",
-        "def multiply(a, b): return a * b"
-    ]
+    def diversify(self, population: list[dict]) -> list[dict]:
+        """
+        Diversify the population by eliminate the greedy algorithms (obj == self.greedy_obj) and adding new ones.
+        """
+        self.iteration += 1
+        
+        # Eliminate greedy algorithms or those with execution errors
+        population = [individual for individual in population if individual["obj"] != self.greedy_obj and individual["exec_success"]]
+        n = self.cfg.pop_size - len(population)
+        logging.info(f"Eliminated {n} greedy algorithms.")
+        
+        if n == 0:
+            return population
+        
+        # Generate new responses
+        messages = [{"role": "system", "content": self.system_prompt}, {"role": "user", "content": self.initial_user + self.code_output_tip}]
+        responses = self.chat_completion(n, self.cfg, messages)
+        
+        # Responses to population
+        new_population = self.responses_to_population(responses)
+        
+        # Run code and evaluate population
+        new_population = self.evaluate_population(new_population)
+        
+        # Add new population to population
+        population.extend(new_population)
+        return population
 
