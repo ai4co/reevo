f6fc13fa7b66d2375ccc25797b68d2964498fb37
Haoran-Ye <hrye@stu.suda.edu.cn>
2023-12-25 20:49:26 +0800

    Update tsp_aco

--- problems/tsp_aco/eval.py
+++ problems/tsp_aco/eval.py
@@ -2,8 +2,9 @@ from aco import ACO
 import sys
 import numpy as np
 from scipy.spatial import distance_matrix
+import logging
 
-from gpt import scoring_function
+from gpt import heuristics_v2 as heuristics
 
 
 N_ITERATIONS = 100
@@ -15,21 +16,42 @@ if __name__ == "__main__":
 
     problem_size = int(sys.argv[1])
     root_dir = sys.argv[2]
+    mood = sys.argv[3]
+    assert mood in ['train', 'val']
     
-    dataset_path = f"{root_dir}/problems/tsp_aco/dataset/val{problem_size}_dataset.npy"
-    node_positions = np.load(dataset_path)
-    n_instances = node_positions.shape[0]
-    print(f"[*] Dataset loaded: {dataset_path} with {n_instances} instances.")
+    if mood == 'train':
+        dataset_path = f"{root_dir}/problems/tsp_aco/dataset/{mood}{problem_size}_dataset.npy"
+        node_positions = np.load(dataset_path)
+        n_instances = node_positions.shape[0]
+        print(f"[*] Dataset loaded: {dataset_path} with {n_instances} instances.")
+        
+        objs = []
+        for i, node_pos in enumerate(node_positions):
+            dist_mat = distance_matrix(node_pos, node_pos)
+            dist_mat[np.diag_indices_from(dist_mat)] = 1 # Note: Set diagonal to a large value
+            heu = heuristics(dist_mat) + 1e-9
+            heu[heu < 1e-9] = 1e-9
+            aco = ACO(dist_mat, heu, n_ants=N_ANTS)
+            obj = aco.run(N_ITERATIONS)
+            print(f"[*] Instance {i}: {obj}")
+            objs.append(obj)
+        
+        print("[*] Average:")
+        print(np.mean(objs))
     
-    objs = []
-    for i, node_pos in enumerate(node_positions):
-        dist_mat = distance_matrix(node_pos, node_pos)
-        dist_mat[np.diag_indices_from(dist_mat)] = np.inf # Note: Set diagonal to inf
-        heuristics = scoring_function(dist_mat) + 1e-9
-        aco = ACO(dist_mat, heuristics, n_ants=N_ANTS)
-        obj = aco.run(N_ITERATIONS)
-        print(f"[*] Instance {i}: {obj}")
-        objs.append(obj)
-    
-    print("[*] Average:")
-    print(np.mean(objs))
+    else:
+        for problem_size in [20, 50, 100]:
+            dataset_path = f"{root_dir}/problems/tsp_aco/dataset/{mood}{problem_size}_dataset.npy"
+            node_positions = np.load(dataset_path)
+            logging.info(f"[*] Evaluating {dataset_path}")
+            n_instances = node_positions.shape[0]
+            objs = []
+            for i, node_pos in enumerate(node_positions):
+                dist_mat = distance_matrix(node_pos, node_pos)
+                dist_mat[np.diag_indices_from(dist_mat)] = 1 # Note: Set diagonal to a large value
+                heu = heuristics(dist_mat) + 1e-9
+                heu[heu < 1e-9] = 1e-9
+                aco = ACO(dist_mat, heu, n_ants=N_ANTS)
+                obj = aco.run(N_ITERATIONS)
+                objs.append(obj.item())
+            print(f"[*] Average for {problem_size}: {np.mean(objs)}")
--- problems/tsp_aco/gen_inst.py
+++ problems/tsp_aco/gen_inst.py
@@ -2,8 +2,13 @@ import numpy as np
 
 np.random.seed(1234)
 
-for problem_size in [20]:
-    n_instances = 10
+for problem_size in [50]:
+    n_instances = 5
+    test_dataset = np.random.rand(n_instances, problem_size, 2)
+    np.save(f'./dataset/train{problem_size}_dataset.npy', test_dataset)
+
+for problem_size in [20, 50, 100]:
+    n_instances = 64
     test_dataset = np.random.rand(n_instances, problem_size, 2)
     np.save(f'./dataset/val{problem_size}_dataset.npy', test_dataset)
     
