71b295f10d0045a72e527e60a83dd2ae01586f15
furffico <jerrywang0206@gmail.com>
2024-01-12 02:05:59 +0800

    [tsp_gls] migrate code

---
+++ problems/tsp_gls/eval.py
@@ -0,0 +1,69 @@
+import numpy as np
+import logging
+from gen_inst import TSPInstance, load_dataset, dataset_conf
+from gls import guided_local_search
+
+try:
+    from gpt import heuristics_v2 as heuristics
+except:
+    from gpt import heuristics
+
+perturbation_moves = 30
+time_limit = 1.0 # seconds
+
+def calculate_cost(inst: TSPInstance, path: np.ndarray) -> float:
+    return inst.distmat[path, np.roll(path, 1)].sum().item()
+
+def solve(inst: TSPInstance) -> float:
+    heu = heuristics(inst.distmat.copy())
+    assert tuple(heu.shape) == (inst.n, inst.n)
+    heu[heu < 1e-6] = 1e-6
+    inittour = np.arange(inst.n) # todo
+    result = guided_local_search(inst.distmat, heu, inittour, perturbation_moves, time_limit)
+    return calculate_cost(inst, result)
+
+if __name__ == "__main__":
+    import sys
+    import os
+
+    print("[*] Running ...")
+
+    problem_size = int(sys.argv[1])
+    mood = sys.argv[3]
+    assert mood in ['train', 'val']
+
+    basepath = os.path.dirname(__file__)
+    # automacially generate dataset if nonexists
+    if not os.path.isfile(os.path.join(basepath, f"dataset/train{dataset_conf['train'][0]}_dataset.npy")):
+        from gen_inst import generate_datasets
+        generate_datasets()
+    
+    if mood == 'train':
+        dataset_path = os.path.join(basepath, f"dataset/{mood}{problem_size}_dataset.npy")
+        dataset = load_dataset(dataset_path)
+        n_instances = dataset[0].n
+
+        print(f"[*] Dataset loaded: {dataset_path} with {n_instances} instances.")
+        
+        objs = []
+        for i, instance in enumerate(dataset):
+            obj = solve(instance)
+            print(f"[*] Instance {i}: {obj}")
+            objs.append(obj)
+        
+        print("[*] Average:")
+        print(np.mean(objs))
+
+    else: # mood == 'val'
+        for problem_size in dataset_conf['val']:
+            dataset_path = os.path.join(basepath, f"dataset/{mood}{problem_size}_dataset.npy")
+            dataset = load_dataset(dataset_path)
+            n_instances = dataset[0].n
+            logging.info(f"[*] Evaluating {dataset_path}")
+
+            objs = []
+            for i, instance in enumerate(dataset):
+                obj = solve(instance)
+                objs.append(obj)
+            
+            print(f"[*] Average for {problem_size}: {np.mean(objs)}")
