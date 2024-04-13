3f74e2b201ce5114f97af9ed0e73359e5c54db93
Haoran-Ye <hrye@stu.suda.edu.cn>
2024-04-13 20:28:52 +0800

    add cvrp_pomo env

---
+++ problems/cvrp_pomo/eval.py
@@ -0,0 +1,152 @@
+##########################################################################################
+# Machine Environment Config
+
+DEBUG_MODE = False
+USE_CUDA = False
+CUDA_DEVICE_NUM = 0
+
+
+##########################################################################################
+# Path Config
+
+import os
+import sys
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.insert(0, "..")  # for problem_def
+# sys.path.insert(0, "../..")  # for utils
+
+
+##########################################################################################
+# import
+
+import logging
+from utils import create_logger, copy_all_src
+
+from CVRPTester import CVRPTester as Tester
+
+
+##########################################################################################
+# parameters
+
+env_params = {
+    'problem_size': 100,
+    'pomo_size': 100,
+}
+
+model_params = {
+    'embedding_dim': 128,
+    'sqrt_embedding_dim': 128**(1/2),
+    'encoder_layer_num': 6,
+    'qkv_dim': 16,
+    'head_num': 8,
+    'logit_clipping': 10,
+    'ff_hidden_dim': 512,
+    'eval_type': 'argmax',
+}
+
+
+tester_params = {
+    'use_cuda': USE_CUDA,
+    'cuda_device_num': CUDA_DEVICE_NUM,
+    'model_load': {
+        'path': './checkpoints/',  # directory path of pre-trained model and log files saved.
+        'epoch': 30500,  # epoch version of pre-trained model to laod.
+    },
+    'test_episodes': 10,
+    'test_batch_size': 10,
+    'augmentation_enable': False,
+    'aug_factor': 8,
+    'aug_batch_size': 400,
+    'test_data_load': {
+        'enable': True,
+        'filename': './vrp100_test_seed1234.pt'
+    },
+}
+if tester_params['augmentation_enable']:
+    tester_params['test_batch_size'] = tester_params['aug_batch_size']
+
+
+logger_params = {
+    'log_file': {
+        'desc': 'test_cvrp100',
+        'filename': 'log.txt'
+    }
+}
+
+
+##########################################################################################
+# main
+
+def main():
+    if DEBUG_MODE:
+        _set_debug_mode()
+
+    # create_logger(**logger_params)
+    # _print_config()
+
+    tester = Tester(env_params=env_params,
+                      model_params=model_params,
+                      tester_params=tester_params)
+
+    # copy_all_src(tester.result_folder)
+
+    avg_obj = tester.run()
+    
+    return avg_obj
+
+
+def _set_debug_mode():
+    global tester_params
+    tester_params['test_episodes'] = 10
+
+
+def _print_config():
+    logger = logging.getLogger('root')
+    logger.info('DEBUG_MODE: {}'.format(DEBUG_MODE))
+    logger.info('USE_CUDA: {}, CUDA_DEVICE_NUM: {}'.format(USE_CUDA, CUDA_DEVICE_NUM))
+    [logger.info(g_key + "{}".format(globals()[g_key])) for g_key in globals().keys() if g_key.endswith('params')]
+
+
+
+##########################################################################################
+
+if __name__ == "__main__":
+    import sys
+    import os
+    from gen_inst import dataset_conf
+
+    print("[*] Running ...")
+
+    problem_size = int(sys.argv[1])
+    mood = sys.argv[3]
+    assert mood in ['train', 'val', "test"]
+    
+    basepath = os.path.dirname(__file__)
+    # automacially generate dataset if nonexists
+    if not os.path.isfile(os.path.join(basepath, f"dataset/train{dataset_conf['train'][0]}_dataset.pt")):
+        from gen_inst import generate_datasets
+        generate_datasets()
+    
+    if not os.path.isfile(os.path.join(basepath, "checkpoints/checkpoint-30500.pt")):
+        raise FileNotFoundError("No checkpoints found. Please see the readme.md and download the checkpoints.")
+
+    if mood == 'train':
+        dataset_path = os.path.join(basepath, f"dataset/{mood}{problem_size}_dataset.pt")
+        tester_params['test_data_load']['filename'] = dataset_path
+        tester_params['test_episodes'] = 10
+        tester_params['test_batch_size'] = 10
+        env_params['problem_size'] = problem_size
+        avg_obj = main()
+        print("[*] Average:")
+        print(avg_obj)
+    
+    else:
+        for problem_size in dataset_conf['val']:
+            dataset_path = os.path.join(basepath, f"dataset/{mood}{problem_size}_dataset.pt")
+            tester_params['test_data_load']['filename'] = dataset_path
+            tester_params['test_episodes'] = 64
+            tester_params['test_batch_size'] = 64
+            env_params['problem_size'] = problem_size
+            avg_obj = main()
+            print(f"[*] Average for {problem_size}: {avg_obj}")
---
+++ problems/cvrp_pomo/gen_inst.py
@@ -0,0 +1,55 @@
+import torch
+
+
+dataset_conf = {
+    'train': (200, 500, 1000),
+    'val':   (200, 500, 1000),
+    'test':  (200, 500, 1000),
+}
+
+
+def get_random_problems(batch_size, problem_size):
+
+    depot_xy = torch.rand(size=(batch_size, 1, 2))
+    # shape: (batch, 1, 2)
+
+    node_xy = torch.rand(size=(batch_size, problem_size, 2))
+    # shape: (batch, problem, 2)
+
+    if problem_size == 20:
+        demand_scaler = 30
+    elif problem_size == 50:
+        demand_scaler = 40
+    elif problem_size == 100:
+        demand_scaler = 50
+    elif problem_size > 100:
+        demand_scaler = 50
+    else:
+        raise NotImplementedError
+
+    node_demand = torch.randint(1, 10, size=(batch_size, problem_size)) / float(demand_scaler)
+    # shape: (batch, problem)
+
+    return depot_xy, node_xy, node_demand
+
+def generate_dataset(filepath, n, batch_size=64):
+    depot_xy, node_xy, node_demand = get_random_problems(batch_size, n)
+    torch.save({
+        'depot_xy': depot_xy,
+        'node_xy': node_xy,
+        'node_demand': node_demand
+    }, filepath)
+
+def generate_datasets(basepath = None):
+    import os
+    basepath = basepath or os.path.join(os.path.dirname(__file__), "dataset")
+    os.makedirs(basepath, exist_ok=True)
+
+    for mood, problem_sizes in dataset_conf.items():
+        torch.manual_seed(len(mood))
+        for n in problem_sizes:
+            filepath = os.path.join(basepath, f"{mood}{n}_dataset.pt")
+            generate_dataset(filepath, n, batch_size=10 if mood =='train' else 64)
+
+if __name__ == "__main__":
+    generate_datasets()
